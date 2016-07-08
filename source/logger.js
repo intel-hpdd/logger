4653deba5fb9238db1a7c14b23548ad651efde65
Joe Grund <joe.grund@intel.com>
2016-07-08 09:46:01 -0400

    HYD-6172 Replace bunyan with custom logger
    Some bunyan deps were blacklisted.
    
    Replace bunyan with a custom logger.
    
    The log format is rather simple, here is an example from the realtime service:
    
    {
      "name":"realtime",
      "hostname":"lotus-30vm13",
      "pid":19872,
      "level":30,
      "sock": {
        "id": "/#z7g2SQ5zGkapWpjBAAAK"
      },
      "msg": "stream ended",
      "time": "2016-07-06T19:12:29.135Z",
      "v":0
    }
    
    Basically it's just a JSON object that is then searchable by bunyan-cli.
    
    If we create a logger that uses the same format,
    we can still use bunyan-cli to search / filter records.
    
      - Create module + tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ source/logger.js
@@ -0,0 +1,98 @@
+//
+// INTEL CONFIDENTIAL
+//
+// Copyright 2013-2016 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related
+// to the source code ("Material") are owned by Intel Corporation or its
+// suppliers or licensors. Title to the Material remains with Intel Corporation
+// or its suppliers and licensors. The Material contains trade secrets and
+// proprietary and confidential information of Intel or its suppliers and
+// licensors. The Material is protected by worldwide copyright and trade secret
+// laws and treaty provisions. No part of the Material may be used, copied,
+// reproduced, modified, published, uploaded, posted, transmitted, distributed,
+// or disclosed in any way without Intel's prior express written permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or delivery
+// of the Materials, either expressly, by implication, inducement, estoppel or
+// otherwise. Any license under such intellectual property rights must be
+// express and approved by Intel in writing.
+
+// @flow
+
+import fs from 'fs';
+import * as fp from 'intel-fp';
+import * as obj from 'intel-obj';
+
+type confT = {
+  name: string,
+  path: string,
+  level: number,
+  serializers: {
+    [key: string]: (x:any) => (boolean | Object)
+  },
+};
+
+export const LEVELS = {
+  INFO: 30,
+  ERROR: 50
+};
+
+export default (config:confT) => {
+  const s = fs.createWriteStream(
+    config.path,
+    {
+      flags: 'a',
+      encoding: 'utf8'
+    }
+  );
+
+  const base = {
+    name: config.name,
+    level: config.level,
+    pid: process.pid,
+    v: 0
+  };
+
+  return createLogger(base, s, config);
+};
+
+
+const createLogger = (base, s, config) => ({
+  child: (r:Object) => {
+    return createLogger({
+      ...base,
+      ...r
+    }, s, config);
+  },
+  info: getLevelLogger(fp.eq(LEVELS.INFO, config.level), s, config.serializers, base),
+  error: getLevelLogger(fp.eq(LEVELS.ERROR, config.level), s, config.serializers, base),
+});
+
+const getLevelLogger = (rightLevel:boolean, s, serializers, base) => {
+  return rightLevel ? parseRecord(s, serializers, base) : () => {};
+};
+
+const parseRecord = (s, serializers, base) => {
+  return (r:Object, msg:string) => {
+    r = {
+      ...base,
+      ...r,
+      msg,
+      time: new Date().toISOString()
+    };
+
+    const result = obj.map((val, key) => {
+      const result = serializers[key] && serializers[key](val);
+
+      return result || val;
+    }, r);
+
+    s
+      .write(
+        JSON
+          .stringify(result)
+      );
+  };
+};
---
+++ test/logger-test.js
@@ -0,0 +1,187 @@
+// @flow
+
+import proxyquire from 'proxyquire';
+
+import {
+  describe,
+  beforeEach,
+  it,
+  jasmine,
+  expect
+} from './jasmine.js';
+
+
+describe('logger', () => {
+  let logger, fs, s, mostRecentWrite;
+
+  beforeEach(() => {
+    s = {
+      write: jasmine.createSpy('write')
+    };
+
+    mostRecentWrite = () => {
+      const mostRecent = s
+        .write
+        .calls
+        .mostRecent()
+        .args[0];
+
+      return JSON.parse(mostRecent);
+    };
+
+    fs = {
+      createWriteStream: jasmine
+        .createSpy('createWriteStream')
+        .and
+        .returnValue(s)
+    };
+
+    logger = proxyquire
+      .noPreserveCache()
+      .noCallThru()('../source/logger', {
+        fs
+      }).default;
+  });
+
+  it('should be a funtion', () => {
+    expect(logger)
+      .toEqual(jasmine.any(Function));
+  });
+
+  describe('creating an instance', () => {
+    let inst;
+
+    beforeEach(() => {
+      inst = logger(
+        {
+          path: 'foo/bar/log.txt',
+          level: 30,
+          name: 'log',
+          serializers: {
+            sock: sock => {
+              if (!sock)
+                return false;
+
+              if (sock === 3)
+                return false;
+
+              return {
+                id: sock.id
+              };
+            }
+          }
+        }
+      );
+    });
+
+    it('should return a logger object', () => {
+      expect(
+        inst
+      )
+      .toEqual({
+        child: jasmine.any(Function),
+        info: jasmine.any(Function),
+        error: jasmine.any(Function)
+      });
+    });
+
+    it('should create a writeStream', () => {
+      expect(fs.createWriteStream)
+        .toHaveBeenCalledOnceWith('foo/bar/log.txt', {
+          flags: 'a',
+          encoding: 'utf8'
+        });
+    });
+
+    it('should log out info', () => {
+      inst.info({
+        foo: 'bar'
+      }, 'It\'s loggin time!');
+
+      const mostRecent = s
+        .write
+        .calls
+        .mostRecent()
+        .args[0];
+
+      expect(JSON.parse(mostRecent))
+        .toEqual({
+          name: 'log',
+          level: 30,
+          pid: jasmine.any(Number),
+          v: 0,
+          foo: 'bar',
+          msg: 'It\'s loggin time!',
+          time: jasmine.any(String)
+        });
+    });
+
+    it('should log nothing for error', () => {
+      inst.error(
+        { foo: 'bar'
+      });
+
+      expect(s.write)
+        .not
+        .toHaveBeenCalled();
+    });
+
+    it('should utilize serializers', () => {
+      inst.info({
+        sock: {
+          id: 5,
+          bar: 'baz'
+        }
+      });
+
+      expect(mostRecentWrite())
+        .toEqual({
+          name: 'log',
+          level: 30,
+          pid: jasmine.any(Number),
+          v: 0,
+          sock: {
+            id: 5
+          },
+          time: jasmine.any(String)
+        });
+    });
+
+    it('should keep existing if serializer does not match', () => {
+      inst.info({
+        sock: 3
+      });
+
+      expect(mostRecentWrite())
+        .toEqual({
+          name: 'log',
+          level: 30,
+          pid: jasmine.any(Number),
+          v: 0,
+          sock: 3,
+          time: jasmine.any(String)
+        });
+    });
+
+    it('should create a child logger', () => {
+      inst
+        .child({
+          fromParent: true
+        })
+        .info({
+          bar: 'baz'
+        });
+
+      expect(mostRecentWrite())
+        .toEqual({
+          name: 'log',
+          level: 30,
+          pid: jasmine.any(Number),
+          v: 0,
+          fromParent: true,
+          bar: 'baz',
+          time: jasmine.any(String)
+        });
+    });
+  });
+});
