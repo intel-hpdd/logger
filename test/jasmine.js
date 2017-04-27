f5bb96df06b3bbadb6846a6675c2fda0b791586a
Joe Grund <grundjoseph@gmail.com>
2017-04-27 13:16:44 -0400

    remove setup-jasmine-env.js + regenerate yarn.lock
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- test/jasmine.js
+++ test/jasmine.js
@@ -1,5 +1,7 @@
 // @flow
 
+import '@mfl/jasmine-n-matchers';
+
 /*global describe beforeEach afterEach it expect jasmine jest:true*/
 
 export { describe, beforeEach, afterEach, it, expect, jasmine, jest };
